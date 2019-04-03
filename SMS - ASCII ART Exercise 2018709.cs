376f76f88162f142d18adf646727f6dd8a8525b1
Shannon <shannoncareer@outlook.com>
2019-04-03 14:25:31 -0500

    Add files via upload
    Initial ASCII Exercise 7_9_2018

---
+++ SMS - ASCII ART Exercise 2018709.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************");
+
+            Console.WriteLine("*   =======   ========  *");
+            Console.WriteLine("    ||   ||   ||    ||   ");
+            Console.WriteLine("*   ||        ||        *");
+            Console.WriteLine("    ||        ||         ");
+            Console.WriteLine("*   =======   ++++++++  *");
+            Console.WriteLine("         ||         ||   ");
+            Console.WriteLine("*        ||         ||  *");
+            Console.WriteLine("    ||   ||   ||    ||   ");
+            Console.WriteLine("*   =======   ========  *");
+
+            Console.WriteLine("*************************");
+
+
+
+
+            Console.WriteLine("           H             ");
+            Console.WriteLine("         H   H           ");
+            Console.WriteLine("       H       H         ");
+            Console.WriteLine("     H           H       ");
+            Console.WriteLine("   H               H     ");
+            Console.WriteLine("HHHHHHHHHHHHHHHHHHHHHHHH ");
+            Console.WriteLine("   H               H     ");
+            Console.WriteLine("   H               H     ");
+            Console.WriteLine("   H               H     ");
+            Console.WriteLine("   H     HHHHH     H     ");
+            Console.WriteLine("   H     H   H     H     ");
+            Console.WriteLine("   H     H   H     H     ");
+            Console.WriteLine("   H     HHHHH     H     ");
+            Console.WriteLine("   H               H     ");
+            Console.WriteLine("   HHHHHHHHHHHHHHHHH     ");
+
+
+
+
+            Console.WriteLine("      A     A                                                                       ");
+            Console.WriteLine("     AAA___AAA                                                                      ");
+            Console.WriteLine("    GDDDDDDDDDDD                                                                    ");
+            Console.WriteLine("   ODDDDDDDDDDDJ                                                                    ");
+            Console.WriteLine("    DDDDDDDDDDD                                                                     ");
+            Console.WriteLine("    FDDDDDDDDDDD                                                                    ");
+            Console.WriteLine("    UDDDDDDDDDD                                                                     ");
+            Console.WriteLine("   OODDDDDDDDDDD                                                                    ");
+            Console.WriteLine("    YOOOOODDDDDDDR                                                                  ");
+            Console.WriteLine("       |DDDDDDDDDDDD                                                                ");
+            Console.WriteLine("       GDDDDDDDDDDDDDD                                                              ");
+            Console.WriteLine("      ODDDDDDDDDDDDDDDDDD                                                           ");
+            Console.WriteLine("       ODDDDDDDDDDDDDDDDDDDD                                          t             ");
+            Console.WriteLine("       |DDDDDDDDDDDDDDDDDDDDDDDDD                                   TT              ");
+            Console.WriteLine("        (DIGITALDOGLOLdddDDDDDDDDDDDDDDDDDD                      RRRR               ");
+            Console.WriteLine("        OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOODDDDDDDDDDDDDDD                   ");
+            Console.WriteLine("       OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                  ");
+            Console.WriteLine("       OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                 ");
+            Console.WriteLine("      KOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO                ");
+            Console.WriteLine("      OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO               ");
+            Console.WriteLine("      OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO              ");
+            Console.WriteLine("      OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO               ");
+            Console.WriteLine("       DDDDDDDDDDDDDDDDDDDDDDddsfsfdsfdafdDDDDDDDDDDDDDDDDDDDDDddddd                ");
+            Console.WriteLine("        TDFDFSFSGADADSFASDFF                    sfdfsafdasfsaffdsad                 ");
+            Console.WriteLine("          fdsfafsafsdfdasfd                       SFDSAFSAFASFSDAFA                 ");
+            Console.WriteLine("           |fdafdasfasfasf|                        fasdfasfasdfassdf                ");
+            Console.WriteLine("            |Ddfadfafdafd|                          DDFDFADFADFADDDD                ");
+            Console.WriteLine("             |DDDDDDDdDD|                            DDDFDFDFDDFDDFDD               ");
+            Console.WriteLine("              |fdfsdafd|                              DFDFDFAFDAFFFDDD              ");
+            Console.WriteLine("               |fdafdf|                                DFDFADFADFAFDAfD             ");
+            Console.WriteLine("               dfadafdf                                  fdafdafdafafddfd           ");
+            Console.WriteLine("                ddfafd                                    DFDADFADFAFDdfda          ");
+            Console.WriteLine("                 dfsfd                                       fsdfadsfafafdd         ");
+            Console.WriteLine("                 fdfda                                          dfafdafdafafs       ");
+            Console.WriteLine("              GGGGGGGG                                        fsfdsfa  fdfafafd     ");
+            Console.WriteLine("             GGG GGGGGG                                      GGGGGGG  GGGGGGGG      ");
+      
+        
+
+            Console.ReadKey();
+        }
+    }
+}
