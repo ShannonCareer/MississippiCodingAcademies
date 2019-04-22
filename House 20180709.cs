39ff51c054b7ab309acfe45eba6d09f9ec7e3812
Shannon <shannoncareer@outlook.com>
2019-04-22 13:59:36 -0500

    ASCII art
    creating a house via ASCII chars

---
+++ House 20180709.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      X      ");
+            Console.WriteLine("     X X     ");
+            Console.WriteLine("    X   X    ");
+            Console.WriteLine("   X     X   ");
+            Console.WriteLine("  X       X  ");
+            Console.WriteLine("XXXXXXXXXXXXX");
+            Console.WriteLine("  X       X  ");
+            Console.WriteLine("  X       X  ");
+            Console.WriteLine("  X       X  ");
+            Console.WriteLine("  XXXXXXXXX  ");
+
+            Console.ReadKey();
+        }
+    }
+}
